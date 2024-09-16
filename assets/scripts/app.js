060de807ff1c906744937fb6b98260a1da398ca0
kaizz04 <kaveesh@marketpushapps.com>
2024-09-16 11:01:04 +0530

    Sending a delete request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -30,6 +31,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
 
     listElement.append(postEl);
   }
@@ -49,9 +51,18 @@ function createPost(title, content) {
 fetchButton.addEventListener("click", () => fetchPosts());
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector('#title').value;
-  const enteredContent = event.currentTarget.querySelector('#content').value;
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
 
-  createPost(enteredTitle,enteredContent);
+  createPost(enteredTitle, enteredContent);
 });
-createPost("DUMMY", "A dummy post!");
+
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
