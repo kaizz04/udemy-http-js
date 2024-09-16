10b5abc2c8f7217453ae9e1b8de20effbd41a882
kaizz04 <kaveesh@marketpushapps.com>
2024-09-16 09:41:26 +0530

    Promisifying Http requests with XMLhr

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,44 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
 
-xhr.responseType = 'json';
-xhr.onload = ()=>{
-   //const listOfPosts = JSON.parse(xhr.response);
-   const listOfPosts = xhr.response;
-   for(const post of listOfPosts){
-    const postEl = document.importNode(postTemplate.content,true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
 
-    listElement.append(postEl);
-   }
+function sendHttpRequest(method, url){
+    const promise = new Promise((resolve,reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method,url);
+        xhr.responseType = 'json';
 
-};
-xhr.send();
+        xhr.onload = ()=>{
+            resolve(xhr.response);
+            //const listOfPosts = JSON.parse(xhr.response);
+          
+         
+         };
+         xhr.send();
+    
+    });
+
+    return promise;
+
+}
+
+async function fetchPosts(){
+    const responseData = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
+ 
+        const listOfPosts = responseData;
+        for(const post of listOfPosts){
+         const postEl = document.importNode(postTemplate.content,true);
+         postEl.querySelector('h2').textContent = post.title.toUpperCase();
+         postEl.querySelector('p').textContent = post.body;
+     
+         listElement.append(postEl);
+        }
+
+}
+
+
+fetchPosts();
 
 
 
