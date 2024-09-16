e266f7b122c92ce3b4840424ef3c2b988b32c661
kaizz04 <kaveesh@marketpushapps.com>
2024-09-16 12:39:49 +0530

    fetch() & error handling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,58 +5,72 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-//   const promise = new Promise((resolve, reject) => {
-    // const xhr = new XMLHttpRequest();
-    // xhr.open(method, url);
-    // xhr.responseType = "json";
+  //   const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // xhr.open(method, url);
+  // xhr.responseType = "json";
 
-    // xhr.onload = () => {
-    //   if(xhr.status >= 200 && xhr.status < 300){
-    //     resolve(xhr.response);
-    //   }else{
-    //     reject(new Error('Something went wrong!'));
-    //   }
-    //   //const listOfPosts = JSON.parse(xhr.response);
-    // };
+  // xhr.onload = () => {
+  //   if(xhr.status >= 200 && xhr.status < 300){
+  //     resolve(xhr.response);
+  //   }else{
+  //     reject(new Error('Something went wrong!'));
+  //   }
+  //   //const listOfPosts = JSON.parse(xhr.response);
+  // };
 
-    // xhr.onerror = ()=>{
-    //     reject(new Error('Failed to send request!'));
-    // };
+  // xhr.onerror = ()=>{
+  //     reject(new Error('Failed to send request!'));
+  // };
 
+  // xhr.send(JSON.stringify(data));
 
-    // xhr.send(JSON.stringify(data));
+  //    });
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json().then((errData) => {
+          throw new Error("Something went wrong -server-side");
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error("Something went wrong!");
+    });
 
-
-//    });
-  return fetch(url).then(response =>{
-    return response.json();
-  });
-
-//   return promise;
+  //   return promise;
 }
 
 async function fetchPosts() {
-    // try {
-        const responseData = await sendHttpRequest(
-            "GET",
-            "https://jsonplaceholder.typicode.com/posts"
-          );
-        
-          const listOfPosts = responseData;
-          for (const post of listOfPosts) {
-            const postEl = document.importNode(postTemplate.content, true);
-            postEl.querySelector("h2").textContent = post.title.toUpperCase();
-            postEl.querySelector("p").textContent = post.body;
-            postEl.querySelector("li").id = post.id;
-        
-            listElement.append(postEl);
-          }
-        
-    // } catch (error) {
-    //     alert(error.message);
-        
-    // }
+  try {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
 
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
+
+    listElement.append(postEl);
+  }
+
+  } catch (error) {
+      alert(error.message);
+
+  }
 }
 
 function createPost(title, content) {
